777660b08eb79f66a8f83d0913afa81070070d14
Alexandr <s3mbl4@gmail.com>
2022-12-04 17:48:26 +0100

    .

--- lesson3/src/Main.java
+++ lesson3/src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
+
+
+    static Scanner scanner = new Scanner(System.in);
+
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+//        int a = scanner.nextInt();
+//        System.out.println(a);
+        int r = scanner.nextInt();
+        int t = scanner.nextInt();
+        int y = scanner.nextInt();
+        System.out.println(sum(r,t,y));
+        System.out.println(minus(r,t));
+    }
+    public static int sum(int a,int b,int c){
+        return a+b+c;
+    }
+    public static int minus(int a,int b){
+        return a-b;
     }
 }
