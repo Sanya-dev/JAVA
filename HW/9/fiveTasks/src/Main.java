9eb2ab4344a6477c1cf45b9608fa9a328733fb19
Alexandr <s3mbl4@gmail.com>
2022-12-25 15:48:16 +0100

    .

--- HW/9/fiveTasks/src/Main.java
+++ HW/9/fiveTasks/src/Main.java
@@ -1,18 +1,93 @@
 import java.util.Random;
 import java.util.Scanner;
 
+/*
+    ДЗ
+    1)Создать Scanner и организовать ввод чисел
+    2) Проверить больше ли некоторого значения сумма двух чисел, если нет вывести сообщение что сумма меньше заданного значение
+    3) Через case обработать несколько вариантов введенной переменной, предусмотреть дефолтный случай
+    4) Инициализировать массив псевдослучайными числами(Random)
+    5) Найти сумму и произведение всех элементов этого массива
+    6) Сделать класс, который будет искать площадь треугольника, прямоугольника и окружности
+     7) В классе Human добавить возможность изменения значений
+     8) Написать 15 тернарных операторов
+    9) Показать работу с методами классов String, StringBuilder и StringBuffer
+    */
+
 public class Main {
     static Random random = new Random();
     static Scanner scanner = new Scanner(System.in);
+
+    static Human human = new Human(29, "Alex");
+    static StringBuffer AAA = new StringBuffer();
     public static void main(String[] args) {
 //      task1();
 //      task2(10);
 //      task3();
 //        task4();
 //        task5();
+        //TASK 6
         Squares squares = new Squares();
-        squares.square();
+        squares.squareTreugolnik();
+        System.out.println("Площадь круга равна " + squares.krug(5));
+        System.out.println("Площадь прямоугольника равна " + squares.pryamougolnik(5,8));
+
+        // Task 7
+         System.out.println("Возраст : " + human.age + " \nИмя : " + human.name);
+         changeHuman();
+
+        //TASK 8
+        // //condition ? value1 : value2
+        // String str = "some string";
+        // boolean a = 2 > 1 ? true : false;
+        // System.out.println(a);
+        // String b = 2 + 2 == 1 + 3 ? "значения равны" : "значения не равны";
+        // System.out.println(b);
+        // boolean c = str.length() > 15 ? true : false;
+        // System.out.println(c);
+        // int hour = 20;
+        // String greeting = hour < 18 ? "Добрый день" : "Добрый вечер";
+        // System.out.println(greeting);
+
+        //TASK 9
+
+        String str = " Some String ";
+        int x = str.length();
+        System.out.println(x);
+
+        str = str.trim();
+        System.out.println(str);
+
+        char chr = str.charAt(2);
+        System.out.println(chr);
+
+        str = str.concat(" stringed");
+        System.out.println(str);
+
+        str = str.toUpperCase();
+        System.out.println(str);
+
+        str = str.toLowerCase();
+        System.out.println(str);
+
+
+        AAA.append("some stringBuffer");
+        System.out.println(AAA);
+
+        AAA.replace(0, 5, "");
+        System.out.println(AAA);
+
+        AAA.reverse();
+        System.out.println(AAA);
+
+        AAA.append(" olleH");
+        System.out.println(AAA);
+
+        AAA.reverse();
+        System.out.println(AAA);
+
     }
+
 
     public static void task1(){
         for (;;) {
@@ -74,4 +149,11 @@ public class Main {
         System.out.println("Сумма массива: " + sum);
         System.out.println("Произведение массива: " + multiply);
     }
+        //TASK 7
+    public static void changeHuman() {
+        human.setAge(30);
+        human.setName("Vova");
+        System.out.println("Возраст : " + human.age + " \nИмя : " + human.name);
+    }
+
 }
--- HW/9/fiveTasks/src/Squares.java
+++ HW/9/fiveTasks/src/Squares.java
@@ -4,12 +4,22 @@ public class Squares {
     private double area;
 
 
-    public void square(){
+    public void squareTreugolnik(){
         System.out.println("Введите высоту треугольника ");
         double height = Main.scanner.nextInt();
         System.out.println("Введите ширину треугольника ");
         double width = Main.scanner.nextInt();
         double area = (height*width)/ 2;
         System.out.println("Площадь треугольника равна " + area);
+    }
+    public double krug(double a) {
+        double s;
+        double pi = 3.14;
+        s = pi * (a * a);
+        return s;
+    }
+
+    public int pryamougolnik(int a, int b) {
+        return a * b;
     }
 }
